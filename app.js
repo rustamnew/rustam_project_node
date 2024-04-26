dc423b4d3647bcabab3f80eaa29286af6bc1ed09
rustamnew <1rustamnew1@gmail.com>
2024-04-26 16:50:32 +0300

    cors config

--- app.js
+++ app.js
@@ -38,10 +38,49 @@ app.use(express.json()) //Использовать json
 
 //app.use(express.static('public'));
 
+const whitelist = ['http://localhost:3000', 'http://rustamproject.ru' ]
+const corsOptions = {
+    credentials: true,
+
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions));
+
+/*
 app.use(cors({
     credentials: true, 
     origin: 'http://localhost:3000'
 }))
+*/
+
+/*
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+*/
 
 app.use(cookieParser())
 
